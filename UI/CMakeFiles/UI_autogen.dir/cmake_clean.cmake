88d333640d4c3978e41ce3111fb936fba7561157
lkobnas <74242572+lkobnas@users.noreply.github.com>
2023-03-31 14:34:35 +0100

    Delete UI directory

--- UI/CMakeFiles/UI_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/UI_autogen"
-  "UI_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/UI_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
