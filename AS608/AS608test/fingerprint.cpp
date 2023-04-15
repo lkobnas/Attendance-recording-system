f40f45b3bfbc0d19b2a9d1a86f54a148c01243b4
HengyuY <1900220590@qq.com>
2023-04-15 18:33:17 +0100

    AS608 code updatd

---
+++ AS608/AS608test/fingerprint.cpp
@@ -0,0 +1,15 @@
+#include "./as608.h"
+#include "./utils.h"
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+class fingerprint{
+    public:
+    
+};
