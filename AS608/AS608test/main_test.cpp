f79c1b03395de286b399f7828634df8cf4b3676b
lkobnas <lkobnas@gmail.com>
2023-04-15 20:40:53 +0100

    added wiringpi to main

--- AS608/AS608test/main_test.cpp
+++ AS608/AS608test/main_test.cpp
@@ -1,8 +1,11 @@
 #include "fingerprint.h"
 #include <iostream>
+#include <wiringPi.h>
 int main(){
     Fingerprint fp;
     int result = fp.fp_init();
     std::cout<< "result:" << result;
+    wiringPiSetup();
+    pinMode(1, INPUT);
     fp.fp_add(9);
 }
