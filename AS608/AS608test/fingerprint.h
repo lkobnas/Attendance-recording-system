1106c2163da517124fd9dec291989fea9d5a81ca
lkobnas <lkobnas@gmail.com>
2023-04-15 20:55:11 +0100

    put all extern varable to main

--- AS608/AS608test/fingerprint.h
+++ AS608/AS608test/fingerprint.h
@@ -15,12 +15,6 @@ class Fingerprint{
     public:
         int fp_init();
         void fp_add(int address);
-        extern AS608 g_as608;
-        extern int g_fd;
-        extern int g_verbose;
-        extern char  g_error_desc[128];
-        extern uchar g_error_code;
-
     private:
 
         int  g_argc = 0;   // 参数个数，g_argc = argc - g_option_count
--- AS608/AS608test/main_test.cpp
+++ AS608/AS608test/main_test.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 #include <wiringPi.h>
 int main(){
+    extern AS608 g_as608;
+    extern int g_fd;
+    extern int g_verbose;
+    extern char  g_error_desc[128];
+    extern uchar g_error_code;
+
     Fingerprint fp;
     int result = fp.fp_init();
     std::cout<< "result:" << result;
-    wiringPiSetup();
-    pinMode(1, INPUT);
+    // wiringPiSetup();
+    // pinMode(1, INPUT);
     fp.fp_add(9);
 }
