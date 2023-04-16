b6e30dddf2f7ecc111543a6101194854a377a1ae
lkobnas <lkobnas@gmail.com>
2023-04-16 20:25:37 +0100

    Testing as608 main_test

--- AS608/main_test.cpp
+++ AS608/main_test.cpp
@@ -2,12 +2,6 @@
 #include <iostream>
 #include <wiringPi.h>
 int main(){
-    extern AS608 g_as608;
-    extern int g_fd;
-    extern int g_verbose;
-    extern char  g_error_desc[128];
-    extern uchar g_error_code;
-
     Fingerprint fp;
     int result = fp.fp_init();
     // fp.syncConfig();
