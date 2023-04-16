ab02fe5e574a38beb018d8e3ff8d5db54a28233e
lkobnas <lkobnas@gmail.com>
2023-04-16 19:02:40 +0100

    updated fingerprint _test

--- AS608/AS608test/main_test.cpp
+++ AS608/AS608test/main_test.cpp
@@ -6,8 +6,8 @@ int main(){
     Fingerprint fp;
     int result = fp.fp_init();
     //std::cout<< "result:" << result<<endl;
-    fp.fp_list();
-    fp.fp_identify();
-    //int fpID = fp.fp_add();
-    //std::cout<< "fpID: "<<fpID;
+    //fp.fp_list();
+    //fp.fp_identify();
+    int fpID = fp.fp_add();
+    std::cout<< "fpID: "<<fpID;
 }
