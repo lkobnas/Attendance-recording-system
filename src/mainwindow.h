2336ddee59cffa9914ff48b3e194d3173594780a
lkobnas <lkobnas@gmail.com>
2023-04-17 16:19:13 +0100

    updated doorControl func

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -93,7 +93,7 @@ private:
 
     RFID rfid;
     Fingerprint fp;
-    Doorlock* doorlock;
+    Doorlock doorlock(26);
     std::thread rfidThread;
     std::thread fingerprintIdentifyThread;
     std::thread doorlockThread;
